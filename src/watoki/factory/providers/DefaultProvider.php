3538dcdf1e55b8c2cab408eb3fcdfb532ad84650
Nikolas Martens <Nikolas.M@rtens.org>
2013-10-25 12:55:20 +0200

    'inject' method was a bad idea [#2]

--- src/watoki/factory/providers/DefaultProvider.php
+++ src/watoki/factory/providers/DefaultProvider.php
@@ -9,26 +9,11 @@ class DefaultProvider implements Provider {
 
     protected $injector;
 
-    private $injectionMethod = 'inject';
-
     public function __construct(Factory $factory) {
         $this->injector = new Injector($factory);
     }
 
     public function provide($class, array $args = array()) {
-        $instance = $this->injector->injectConstructor($class, $args);
-
-        if ($this->injectionMethod && method_exists($instance, $this->injectionMethod)) {
-            $this->injector->injectMethod($instance, $this->injectionMethod);
-        }
-
-        return $instance;
-    }
-
-    /**
-     * @param string $injectionMethod
-     */
-    public function setInjectionMethod($injectionMethod) {
-        $this->injectionMethod = $injectionMethod;
+        return $this->injector->injectConstructor($class, $args);
     }
 }
