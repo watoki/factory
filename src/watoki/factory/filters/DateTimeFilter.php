0639d931c08772b50b69ed2ea2fb0cf72465b9dc
Nikolas Martens <Nikolas.M@rtens.org>
2014-09-05 20:59:02 +0200

    Don't inflate null as DateTime('now')

--- src/watoki/factory/filters/DateTimeFilter.php
+++ src/watoki/factory/filters/DateTimeFilter.php
@@ -6,6 +6,9 @@ use watoki\factory\Filter;
 class DateTimeFilter implements Filter {
 
     public function filter($value) {
+        if (!$value) {
+            return null;
+        }
         return new \DateTime($value);
     }
 }
