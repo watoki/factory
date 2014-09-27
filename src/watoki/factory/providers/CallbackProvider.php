abddf957c5493e96a77f734130c12295658ecd9e
Nikolas Martens <Nikolas.M@rtens.org>
2014-09-27 10:06:34 +0200

    Added CallbackProvider for creating anonymous providers

---
+++ src/watoki/factory/providers/CallbackProvider.php
@@ -0,0 +1,18 @@
+<?php
+namespace watoki\factory\providers;
+
+use watoki\factory\Provider;
+
+class CallbackProvider implements Provider {
+
+    /** @var callable */
+    private $callback;
+
+    public function __construct($callback) {
+        $this->callback = $callback;
+    }
+
+    public function provide($class, array $args = array()) {
+        call_user_func($this->callback, $class, $args);
+    }
+}
