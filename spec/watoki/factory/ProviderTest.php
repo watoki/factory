5382fd73516f3a47f75bfdd5b8c68ccd917263dc
Nikolas Martens <Nikolas.M@rtens.org>
2015-05-12 12:34:15 +0200

    Typo

--- spec/watoki/factory/ProviderTest.php
+++ spec/watoki/factory/ProviderTest.php
@@ -6,40 +6,40 @@ use watoki\scrut\Specification;
 /**
  * Providers can be used to create instances of classes on-demand.
  *
- * @property \spec\watoki\factory\FactoryFixture factoryFix <-
+ * @property \spec\watoki\factory\FactoryFixture $fix <-
  */
 class ProviderTest extends Specification {
 
     public function testFindProviderDirectly() {
-        $this->factoryFix->givenTheClassDefinition('class MyClass {}');
+        $this->fix->givenTheClassDefinition('class MyClass {}');
         $this->givenTheProvider_Providing('SingleMindedProvider', 'return json_decode(\'{"nothing":"just this"}\');');
         $this->givenIHaveSet_ToProvideFor('SingleMindedProvider', 'MyClass');
 
-        $this->factoryFix->whenIGet_FromTheFactory('MyClass');
+        $this->fix->whenIGet_FromTheFactory('MyClass');
 
-        $this->factoryFix->thenTheObjectShouldBeAnInstanceOf('stdClass');
-        $this->factoryFix->thenTheTheProperty_OfTheObjectShouldBe('nothing', 'just this');
+        $this->fix->thenTheObjectShouldBeAnInstanceOf('stdClass');
+        $this->fix->thenTheTheProperty_OfTheObjectShouldBe('nothing', 'just this');
     }
 
     public function testFindBaseProvider() {
-        $this->factoryFix->givenTheClassDefinition('
+        $this->fix->givenTheClassDefinition('
             class BaseClass {}
         ');
-        $this->factoryFix->givenTheClassDefinition('
+        $this->fix->givenTheClassDefinition('
             class SubClass extends BaseClass {}
         ');
 
         $this->givenTheProvider_Providing('BaseProvider', '$o = new $class; $o->provided = "yes"; return $o;');
         $this->givenIHaveSet_ToProvideFor('BaseProvider', 'BaseClass');
 
-        $this->factoryFix->whenIGet_FromTheFactory('SubClass');
+        $this->fix->whenIGet_FromTheFactory('SubClass');
 
-        $this->factoryFix->thenTheObjectShouldBeAnInstanceOf('SubClass');
-        $this->factoryFix->thenTheTheProperty_OfTheObjectShouldBe('provided', 'yes');
+        $this->fix->thenTheObjectShouldBeAnInstanceOf('SubClass');
+        $this->fix->thenTheTheProperty_OfTheObjectShouldBe('provided', 'yes');
     }
 
     public function testFindProviderForInterface() {
-        $this->factoryFix->givenTheClassDefinition('
+        $this->fix->givenTheClassDefinition('
             interface BaseInterface {}
             interface SomeInterface {}
             interface SomeOtherInterface extends BaseInterface {}
@@ -49,16 +49,16 @@ class ProviderTest extends Specification {
         $this->givenTheProvider_Providing('InterfaceProvider', 'return new \DateTime;');
         $this->givenIHaveSet_ToProvideFor('InterfaceProvider', 'BaseInterface');
 
-        $this->factoryFix->whenIGet_FromTheFactory('SomeImplementation');
+        $this->fix->whenIGet_FromTheFactory('SomeImplementation');
 
-        $this->factoryFix->thenTheObjectShouldBeAnInstanceOf('DateTime');
+        $this->fix->thenTheObjectShouldBeAnInstanceOf('DateTime');
     }
 
     public function testFindSpecificProviderFirst() {
-        $this->factoryFix->givenTheClassDefinition('
+        $this->fix->givenTheClassDefinition('
             class Base2Class {}
         ');
-        $this->factoryFix->givenTheClassDefinition('
+        $this->fix->givenTheClassDefinition('
             class Sub2Class extends BaseClass {}
         ');
 
@@ -68,25 +68,25 @@ class ProviderTest extends Specification {
         $this->givenTheProvider_Providing('FirstProvider', '$o = new $class; $o->provided = "first"; return $o;');
         $this->givenIHaveSet_ToProvideFor('FirstProvider', 'Sub2Class');
 
-        $this->factoryFix->whenIGet_FromTheFactory('Sub2Class');
+        $this->fix->whenIGet_FromTheFactory('Sub2Class');
 
-        $this->factoryFix->thenTheObjectShouldBeAnInstanceOf('Sub2Class');
-        $this->factoryFix->thenTheTheProperty_OfTheObjectShouldBe('provided', 'first');
+        $this->fix->thenTheObjectShouldBeAnInstanceOf('Sub2Class');
+        $this->fix->thenTheTheProperty_OfTheObjectShouldBe('provided', 'first');
     }
 
     public function testNormalizeClassName() {
-        $this->factoryFix->givenTheClassDefinition('namespace My; class FooClass {}');
+        $this->fix->givenTheClassDefinition('namespace My; class FooClass {}');
         $this->givenTheProvider_Providing('JustAProvider', 'return json_decode(\'{"nothing":"just this"}\');');
         $this->givenIHaveSet_ToProvideFor('JustAProvider', '\My\FooClass');
 
-        $this->factoryFix->whenIGet_FromTheFactory('my\fooclass');
+        $this->fix->whenIGet_FromTheFactory('my\fooclass');
 
-        $this->factoryFix->thenTheObjectShouldBeAnInstanceOf('stdClass');
-        $this->factoryFix->thenTheTheProperty_OfTheObjectShouldBe('nothing', 'just this');
+        $this->fix->thenTheObjectShouldBeAnInstanceOf('stdClass');
+        $this->fix->thenTheTheProperty_OfTheObjectShouldBe('nothing', 'just this');
     }
 
     private function givenTheProvider_Providing($providerName, $statement) {
-        $this->factoryFix->givenTheClassDefinition("
+        $this->fix->givenTheClassDefinition("
             class $providerName implements \\watoki\\factory\\Provider {
                 public function provide(\$class, array \$args = array()) {
                     $statement
@@ -96,7 +96,7 @@ class ProviderTest extends Specification {
     }
 
     private function givenIHaveSet_ToProvideFor($provider, $class) {
-        $this->factoryFix->factory->setProvider($class, new $provider);
+        $this->fix->factory->setProvider($class, new $provider);
     }
 
 }
